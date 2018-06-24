48929c7f961819b36a6e1ea75760bf444399ab31
Max Magee <maxmagee@users.noreply.github.com>
2018-06-24 11:41:50 -0500

    Icon on home screen animates when keyboard is shown or hidden.

--- currency-converter/app/components/Logo/Logo.js
+++ currency-converter/app/components/Logo/Logo.js
@@ -1,17 +1,70 @@
-import React from 'react';
-import { Image, ImageBackground, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Animated, Keyboard, Platform, Text, View } from 'react-native';
 
-import backgroundImage from './images/background.png';
-import logoImage from './images/logo.png';
+import logoWithBackgroundImage from './images/logoWithBackground.png';
 import styles from './styles';
 
-const Logo = () => (
-  <View style={styles.container}>
-    <ImageBackground resizeMode="contain" style={styles.containerImage} source={backgroundImage}>
-      <Image resizeMode="contain" style={styles.image} source={logoImage} />
-    </ImageBackground>
-    <Text style={styles.text}>Currency Converter</Text>
-  </View>
-);
+const ANIMATION_DURATION = 250;
+
+class Logo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.containerImageWidth = new Animated.Value(styles.$largeContainerSize);
+    this.imageWidth = new Animated.Value(styles.$largeImageSize);
+  }
+
+  componentWillMount() {}
+
+  componentDidMount() {
+    let showListener = 'keyboardWillShow';
+    let hideListener = 'keyboardWillHide';
+
+    if (Platform.OS === 'android') {
+      showListener = 'keyboardDidShow';
+      hideListener = 'keyboardDidHide';
+    }
+
+    this.keyboardShowListener = Keyboard.addListener(showListener, this.keyboardShow);
+    this.keyboardHideListener = Keyboard.addListener(hideListener, this.keyboardHide);
+  }
+
+  componentWillUnmount() {
+    this.keyboardShowListener.remove();
+    this.keyboardHideListener.remove();
+  }
+
+  keyboardShow = () => {
+    Animated.timing(this.containerImageWidth, {
+      toValue: styles.$smallContainerSize,
+      duration: ANIMATION_DURATION
+    }).start();
+  };
+
+  keyboardHide = () => {
+    Animated.timing(this.containerImageWidth, {
+      toValue: styles.$largeContainerSize,
+      duration: ANIMATION_DURATION
+    }).start();
+  };
+
+  render() {
+    const containerImageStyle = [
+      styles.containerImage,
+      { width: this.containerImageWidth, height: this.containerImageWidth }
+    ];
+
+    return (
+      <View style={styles.container}>
+        <Animated.Image
+          resizeMode="contain"
+          style={containerImageStyle}
+          source={logoWithBackgroundImage}
+        />
+        <Text style={styles.text}>Currency Converter</Text>
+      </View>
+    );
+  }
+}
 
 export default Logo;
